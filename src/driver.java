5d3938211dbf0868dca2496471994fcc2e3d9896
kalyanrohan <sri.rohan@binus.ac.id>
2021-05-07 11:31:55 +0700

    Initial commit

---
+++ src/driver.java
@@ -0,0 +1,5 @@
+public class driver {
+    public static void main(String[] args){
+        //1. Genus is the Parent class and Species is the child class. Species inherits from the Genus class
+    }
+}
